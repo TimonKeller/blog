00de8680c860ea6b45c1bd06c73164b05fc65df8
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-06-22 21:53:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ module.exports = {
   reactStrictMode: true,
   env:{
     mapbox_key:'pk.eyJ1IjoidGltb24ta2VsbGVyIiwiYSI6ImNsNGN2OTNoczAyZGozanFobmw0d3ZtNnAifQ.l_AyQZsG2g1-SX9tXR0XAg'
-  }
+  },  
 }
