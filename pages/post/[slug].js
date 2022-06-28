c6ab7319c0c97d7701171b9eb15011ad60ac608d
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-06-28 23:25:47 +0200

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -12,7 +12,7 @@ const Post = ({post, recentPosts}) => {
             <div className='lg:col-span-7 col-span-1'>
             {post.map((post, index) => <Newestpost newPost={post} key={post.title}/>)}
             </div>
-            <div className='lg:col-span-5  col-span-1 px-20'>
+            <div className='lg:col-span-5  col-span-1 px-10'>
               <div className='lg:sticky top-8 mt-12'>
                 <p className='lg:sticky text-xl border-b-2 border-text inline-block mb-12'>Vergangene Posts</p>
                 {recentPosts.map((recentPosts, index) => <Recentposts recentPosts={recentPosts} key={recentPosts.title}/>)}
