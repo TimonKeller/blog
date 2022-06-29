a49e243c9895781610a6d43774ee624c29e65574
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-06-29 10:45:15 +0200

    reformatting

--- components/AllPosts.jsx
+++ components/AllPosts.jsx
@@ -1,45 +1,42 @@
-import React from 'react'
-import moment from 'moment';
-import Link from 'next/link';
+import React from "react";
+import moment from "moment";
+import Link from "next/link";
 
-const AllPosts = ({allPosts}) => {
+const AllPosts = ({ allPosts }) => {
   return (
-    <div className='pb-12'>
+    <div className="pb-12">
       <div>
         <div>
           <div>
-            <div className='text-xs mb'>
-              {moment(allPosts.createdAt).format('MMM DD, YYYY')}
+            <div className="text-xs mb">
+              {moment(allPosts.createdAt).format("MMM DD, YYYY")}
             </div>
-            {<img 
-            src={allPosts.contentfoto.url}
-            alt={allPosts.title}
-            className="object-top w-full h-52 lg:h-64 md:h-64 object-cover py-4"
-            loading='lazy'
-            />}
-            <div className='text-2xl mb-2'>
-              {allPosts.title}
-            </div>
-          </div>  
-        </div>
-        <div className='line_clamp'>
-          {allPosts.teaser}
-        </div>
-            {allPosts.categories.map((category) => {
-            category.name
-            console.log(category.name)
-          })}
-          <div className='cursor-pointer pt-2 text-2xs '>
-            <Link href={`/post/${allPosts.slug}`} key={allPosts.title}>
-                <p className='border-b-2 hover:border-black inline-block'>
-                  Mehr lesen
-                </p>
-            </Link>
+            {
+              <img
+                src={allPosts.contentfoto.url}
+                alt={allPosts.title}
+                className="object-top w-full h-52 lg:h-64 md:h-64 object-cover py-4"
+                loading="lazy"
+              />
+            }
+            <div className="text-2xl mb-2">{allPosts.title}</div>
           </div>
+        </div>
+        <div className="line_clamp">{allPosts.teaser}</div>
+        {allPosts.categories.map((category) => {
+          category.name;
+          console.log(category.name);
+        })}
+        <div className="cursor-pointer pt-2 text-2xs ">
+          <Link href={`/post/${allPosts.slug}`} key={allPosts.title}>
+            <p className="border-b-2 hover:border-black inline-block">
+              Mehr lesen
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllPosts
-
+export default AllPosts;
